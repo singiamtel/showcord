4dbc2ba12335049bafb084a6344ad54f8c661c48
Sergio <sergio@garciadelacruz.es>
2023-08-23 15:05:41 +0200

    Shuffle everything a bit

--- src/client/client.ts
+++ src/client/client.ts
@@ -13,12 +13,12 @@ export class Client {
   challstr: string = "";
   rooms: Room[] = [];
   events: EventTarget = new EventTarget();
-  private joinAfterLogin: string[] = [];
   username: string = "";
-  private cleanUsername: string = "";
   loggedIn: boolean = false;
   settings: Settings = new Settings();
-  onOpen: (() => void)[] = [];
+  onOpen: (() => void)[] = []; // Append stuff here to run when the socket opens
+  private joinAfterLogin: string[] = [];
+  private cleanUsername: string = "";
   private selectedRoom: string = "";
 
   constructor() {
@@ -26,40 +26,272 @@ export class Client {
     this.__setupSocketListeners();
   }
 
-  // Order of login methods:
-  // 1. Assertion in URL (from oauth login)
-  // - This happens right after oauth login
-  // - We also need to store the token in localforage
-  //
-  // 2. Assertion from token
-  // - This happens when we have a token stored in localforage
-  // - We try to get an assertion from the token, and send it to the server
-  // - If it fails we drop the token and go to #3
-  //
-  // 3. Normal login
-  // Redirect to oauth login page
-
-  private __setupSocketListeners() {
-    this.socket.onopen = () => {
-      console.log("socket connected");
-      for (let cb of this.onOpen) {
-        cb();
-      }
-      this.tryLogin();
-    };
-    this.socket.onmessage = (event) => {
-      console.log("msg:", event.data);
-      this.parseSocketMsg(event.data);
-    };
-    this.socket.onerror = (event) => {
-      console.error(event);
-    };
-    this.socket.onclose = (_) => {
-      this.__setupSocketListeners();
-      this.events.dispatchEvent(new CustomEvent("disconnect"));
-    };
+  async send(room: string, message: string) {
+    console.log(`${room}|${message}`);
+    this.socket.send(`${room}|${message}`);
   }
 
+  room(room_id: string) {
+    return this.rooms.find((room) => room.ID === room_id);
+  }
+
+  // Used to remove highlights and mentions
+  selectRoom(roomid: string) {
+    this.selectedRoom = roomid;
+    this.room(roomid)?.select();
+  }
+
+  leaveRoom(room_id: string) {
+    this.socket.send(`|/leave ${room_id}`);
+  }
+
+  async join(rooms: string | string[], useDefaultRooms = false) {
+    console.log("joining rooms...", rooms);
+    if (useDefaultRooms && (!rooms || rooms.length === 0)) {
+      for (let room of this.settings.defaultRooms) {
+        this.socket.send(`|/join ${room}`);
+      }
+      return;
+    }
+
+    if (typeof rooms === "string") {
+      this.socket.send(`|/join ${rooms}`);
+    } else {
+      for (let room of rooms) {
+        this.socket.send(`|/join ${room}`);
+      }
+    }
+  }
+
+  private highlightMsg(roomid: string, message: string) {
+    if (
+      this.cleanUsername && (message.includes(this.cleanUsername) ||
+        message.includes(toID(this.username)))
+    ) {
+      return true;
+    }
+    if (
+      this.settings.highlightMsg(roomid, message)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+
+  // --- Login ---
+
+  async login() {
+    // Order of login methods:
+    // 1. Assertion in URL (from oauth login)
+    // - This happens right after oauth login
+    // - We also need to store the token in localforage
+    //
+    // 2. Assertion from token
+    // - This happens when we have a token stored in localforage
+    // - We try to get an assertion from the token, and send it to the server
+    // - If it fails we drop the token and go to #3
+    //
+    // 3. Normal login
+    // Redirect to oauth login page
+    while (!this.challstr) {
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    // Oauth login method
+    const url =
+      `https://play.pokemonshowdown.com/api/oauth/authorize?redirect_uri=${location.origin}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}&challenge=${this.challstr}`;
+    console.log("url", url);
+    const nWindow = (window as any).n = open(
+      url,
+      undefined,
+      "popup=1,width=700,height=700",
+    );
+    const checkIfUpdated = async () => {
+      try {
+        if (nWindow?.location.host === location.host) {
+          const url = new URL(nWindow.location.href);
+          console.log("URL", url);
+          const assertion = url.searchParams.get("assertion");
+          if (assertion) {
+            this.send_assertion(assertion);
+          }
+          const token = url.searchParams.get("token");
+          if (token) {
+            await localforage.setItem(
+              "ps-token",
+              url.searchParams.get("token"),
+            );
+          }
+          nWindow.close();
+          console.log("got token", token);
+        } else {
+          setTimeout(checkIfUpdated, 500);
+        }
+      } catch (e) {
+        // DomException means that the window wasn't redirected yet
+        // so we just wait a bit more
+        if (e instanceof DOMException) {
+          setTimeout(checkIfUpdated, 500);
+          return;
+        }
+        throw e;
+      }
+    };
+    setTimeout(checkIfUpdated, 1000);
+  }
+
+  private async tryLogin() {
+    console.log("trying to login");
+    while (!this.challstr) {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+    const urlParams = new URLSearchParams(window.location.search);
+    let assertion = urlParams.get("assertion");
+    if (assertion && assertion !== "undefined") {
+      console.log("logging in with assertion", assertion);
+      await this.send_assertion(assertion);
+      const token = urlParams.get("token");
+      if (token) {
+        await localforage.setItem("ps-token", token);
+      }
+      return;
+    } else if (
+      (assertion = await this.assertionFromToken(this.challstr) || null)
+    ) {
+      console.log("logging in with token+assertion", assertion);
+      await this.send_assertion(assertion);
+      return;
+    }
+  }
+
+  private async send_assertion(assertion: string) {
+    //
+    console.log("sending assertion", assertion);
+    const username = assertion.split(",")[1];
+    console.log("trn", username, assertion);
+    this.socket.send(`|/trn ${username},0,${assertion}`);
+  }
+
+  private async parseLoginserverResponse(
+    response: Response,
+  ): Promise<string | false> {
+    // Loginserver responses are just weird
+    const response_test = await response.text();
+    console.log("response_test", response_test);
+    if (response_test[0] === ";") {
+      console.error("AssertionError: Received ; from loginserver");
+      return false;
+    }
+    try {
+      const response_json = JSON.parse(response_test.slice(1));
+      console.log("token response_json", response_json);
+      if (response_json.success === false) {
+        console.error(`Couldn't login`, response_json);
+        return false;
+      } else if (response_json.success) {
+        return response_json.success;
+      }
+    } catch (e) {
+    }
+    return response_test;
+  }
+
+  private async assertionFromToken(challstr: string): Promise<string | false> {
+    const token = await localforage.getItem("ps-token");
+    if (!token || token === "undefined") {
+      console.log("no token");
+      return false;
+    }
+    const response = await fetch(
+      `${this.loginserver_url}oauth/api/getassertion?challenge=${challstr}&token=${token}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}`,
+    );
+    return await this.parseLoginserverResponse(response);
+  }
+
+  // TODO: Actually use this
+  private async refreshToken() {
+    const token = await localforage.getItem("ps-token");
+    if (!token || token === "undefined") {
+      console.log("no token");
+      return null;
+    }
+    try {
+      const response = await fetch(
+        `${this.loginserver_url}oauth/api/refreshtoken?token=${token}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}`,
+      );
+      const result = await this.parseLoginserverResponse(response);
+      console.log("refreshed token", result);
+      if (result) await localforage.setItem("ps-token", result);
+      return result;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
+  }
+
+
+  // --- Room management ---
+  private _addRoom(room: Room) {
+    this.rooms.push(room);
+    const eventio = new CustomEvent("room", { detail: room });
+    this.events.dispatchEvent(eventio);
+    this.settings.changeRooms(this.rooms);
+  }
+
+  private _removeRoom(room_id: string) {
+    this.rooms = this.rooms.filter((room) => room.ID !== room_id);
+    const eventio = new CustomEvent("leaveroom", { detail: room_id });
+    this.events.dispatchEvent(eventio);
+    this.settings.changeRooms(this.rooms);
+  }
+
+  private addMessage(room_id: string, message: Message) {
+    const room = this.room(room_id);
+    if (this.highlightMsg(room_id, message.content)) {
+      message.hld = true;
+    }
+    if (room) {
+      room.addMessage(message);
+      this.events.dispatchEvent(
+        new CustomEvent("message", { detail: message }),
+      );
+      return;
+    }
+    console.log("room (" + room_id + ") does not exist");
+  }
+
+  private addUsers(room_id: string, users: User[]) {
+    const room = this.room(room_id);
+    if (room) {
+      room.addUsers(users);
+      this.events.dispatchEvent(new CustomEvent("users", { detail: users }));
+      return;
+    }
+    console.log("room (" + room_id + ") does not exist");
+  }
+
+
+  private setUsername(username: string) {
+    // gotta re-run highlightMsg on all messages
+    this.username = username;
+    this.cleanUsername = username.replace(/[\u{0080}-\u{FFFF}]/gu, "").trim();
+    this.rooms.forEach(async (room) => {
+      room.messages.forEach((msg) => {
+        console.log("trying to hl", msg);
+        if (this.highlightMsg(room.ID, msg.content)) {
+          msg.hld = true;
+        } else {
+          msg.hld = false;
+        }
+      });
+    });
+    this.events.dispatchEvent(
+      new CustomEvent("login", { detail: this.username }),
+    );
+  }
+
+  // --- Commands parser ---
+  // Hopefully this code will become cleaner with time (lol)
   private async parseSocketMsg(message: string) {
     if (message.startsWith("|challstr|")) {
       const splitted_challstr = message.split("|");
@@ -244,250 +476,24 @@ export class Client {
     });
   }
 
-  room(room_id: string) {
-    return this.rooms.find((room) => room.ID === room_id);
-  }
-
-  leaveRoom(room_id: string) {
-    this.socket.send(`|/leave ${room_id}`);
-  }
-
-  private _addRoom(room: Room) {
-    this.rooms.push(room);
-    const eventio = new CustomEvent("room", { detail: room });
-    this.events.dispatchEvent(eventio);
-    this.settings.changeRooms(this.rooms);
-  }
-
-  private _removeRoom(room_id: string) {
-    this.rooms = this.rooms.filter((room) => room.ID !== room_id);
-    const eventio = new CustomEvent("leaveroom", { detail: room_id });
-    this.events.dispatchEvent(eventio);
-    this.settings.changeRooms(this.rooms);
-  }
-
-  private addMessage(room_id: string, message: Message) {
-    const room = this.room(room_id);
-    if (this.highlightMsg(room_id, message.content)) {
-      message.hld = true;
-    }
-    if (room) {
-      room.addMessage(message);
-      this.events.dispatchEvent(
-        new CustomEvent("message", { detail: message }),
-      );
-      return;
-    }
-    console.log("room (" + room_id + ") does not exist");
-  }
-
-  private addUsers(room_id: string, users: User[]) {
-    const room = this.room(room_id);
-    if (room) {
-      room.addUsers(users);
-      this.events.dispatchEvent(new CustomEvent("users", { detail: users }));
-      return;
-    }
-    console.log("room (" + room_id + ") does not exist");
-  }
-
-  private async tryLogin() {
-    console.log("trying to login");
-    while (!this.challstr) {
-      await new Promise((resolve) => setTimeout(resolve, 500));
-    }
-    const urlParams = new URLSearchParams(window.location.search);
-    let assertion = urlParams.get("assertion");
-    if (assertion && assertion !== "undefined") {
-      console.log("logging in with assertion", assertion);
-      await this.send_assertion(assertion);
-      const token = urlParams.get("token");
-      if (token) {
-        await localforage.setItem("ps-token", token);
+  private __setupSocketListeners() {
+    this.socket.onopen = () => {
+      console.log("socket connected");
+      for (let cb of this.onOpen) {
+        cb();
       }
-      return;
-    } else if (
-      (assertion = await this.assertionFromToken(this.challstr) || null)
-    ) {
-      console.log("logging in with token+assertion", assertion);
-      await this.send_assertion(assertion);
-      return;
-    }
-  }
-
-  private async send_assertion(assertion: string) {
-    //
-    console.log("sending assertion", assertion);
-    const username = assertion.split(",")[1];
-    console.log("trn", username, assertion);
-    this.socket.send(`|/trn ${username},0,${assertion}`);
-  }
-
-  private async parseLoginserverResponse(
-    response: Response,
-  ): Promise<string | false> {
-    // Loginserver responses are just weird
-    const response_test = await response.text();
-    console.log("response_test", response_test);
-    if (response_test[0] === ";") {
-      console.error("AssertionError: Received ; from loginserver");
-      return false;
-    }
-    try {
-      const response_json = JSON.parse(response_test.slice(1));
-      console.log("token response_json", response_json);
-      if (response_json.success === false) {
-        console.error(`Couldn't login`, response_json);
-        return false;
-      } else if (response_json.success) {
-        return response_json.success;
-      }
-    } catch (e) {
-    }
-    return response_test;
-  }
-
-  private async assertionFromToken(challstr: string): Promise<string | false> {
-    const token = await localforage.getItem("ps-token");
-    if (!token || token === "undefined") {
-      console.log("no token");
-      return false;
-    }
-    const response = await fetch(
-      `${this.loginserver_url}oauth/api/getassertion?challenge=${challstr}&token=${token}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}`,
-    );
-    return await this.parseLoginserverResponse(response);
-  }
-
-  // TODO: Actually use this
-  private async refreshToken() {
-    const token = await localforage.getItem("ps-token");
-    if (!token || token === "undefined") {
-      console.log("no token");
-      return null;
-    }
-    try {
-      const response = await fetch(
-        `${this.loginserver_url}oauth/api/refreshtoken?token=${token}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}`,
-      );
-      const result = await this.parseLoginserverResponse(response);
-      console.log("refreshed token", result);
-      if (result) await localforage.setItem("ps-token", result);
-      return result;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  }
-
-  async login() {
-    while (!this.challstr) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
-    }
-    // Oauth login method
-    const url =
-      `https://play.pokemonshowdown.com/api/oauth/authorize?redirect_uri=${location.origin}&client_id=${process.env.NEXT_PUBLIC_OAUTH_ID}&challenge=${this.challstr}`;
-    console.log("url", url);
-    const nWindow = (window as any).n = open(
-      url,
-      undefined,
-      "popup=1,width=700,height=700",
-    );
-    const checkIfUpdated = async () => {
-      try {
-        if (nWindow?.location.host === location.host) {
-          const url = new URL(nWindow.location.href);
-          console.log("URL", url);
-          const assertion = url.searchParams.get("assertion");
-          if (assertion) {
-            this.send_assertion(assertion);
-          }
-          const token = url.searchParams.get("token");
-          if (token) {
-            await localforage.setItem(
-              "ps-token",
-              url.searchParams.get("token"),
-            );
-          }
-          nWindow.close();
-          console.log("got token", token);
-        } else {
-          setTimeout(checkIfUpdated, 500);
-        }
-      } catch (e) {
-        // DomException means that the window wasn't redirected yet
-        // so we just wait a bit more
-        if (e instanceof DOMException) {
-          setTimeout(checkIfUpdated, 500);
-          return;
-        }
-        throw e;
-      }
+      this.tryLogin();
     };
-    setTimeout(checkIfUpdated, 1000);
-  }
-
-  async join(rooms: string | string[], useDefaultRooms = false) {
-    console.log("joining rooms...", rooms);
-    if (useDefaultRooms && (!rooms || rooms.length === 0)) {
-      for (let room of this.settings.defaultRooms) {
-        this.socket.send(`|/join ${room}`);
-      }
-      return;
-    }
-
-    if (typeof rooms === "string") {
-      this.socket.send(`|/join ${rooms}`);
-    } else {
-      for (let room of rooms) {
-        this.socket.send(`|/join ${room}`);
-      }
-    }
-  }
-
-  async send(room: string, message: string) {
-    console.log(`${room}|${message}`);
-    this.socket.send(`${room}|${message}`);
-  }
-
-  highlightMsg(roomid: string, message: string) {
-    if (
-      this.cleanUsername && (message.includes(this.cleanUsername) ||
-        message.includes(toID(this.username)))
-    ) {
-      console.log("trueee");
-      return true;
-    }
-    if (
-      this.settings.highlightMsg(roomid, message)
-    ) {
-      console.log("highlighted message", message);
-      return true;
-    }
-    return false;
-  }
-
-  private setUsername(username: string) {
-    // gotta re-run highlightMsg on all messages
-    this.username = username;
-    this.cleanUsername = username.replace(/[\u{0080}-\u{FFFF}]/gu, "").trim();
-    this.rooms.forEach(async (room) => {
-      room.messages.forEach((msg) => {
-        console.log("trying to hl", msg);
-        if (this.highlightMsg(room.ID, msg.content)) {
-          msg.hld = true;
-        } else {
-          msg.hld = false;
-        }
-      });
-    });
-    this.events.dispatchEvent(
-      new CustomEvent("login", { detail: this.username }),
-    );
-  }
-
-  selectRoom(roomid: string) {
-    this.selectedRoom = roomid;
-    this.room(roomid)?.select();
+    this.socket.onmessage = (event) => {
+      console.log("msg:", event.data);
+      this.parseSocketMsg(event.data);
+    };
+    this.socket.onerror = (event) => {
+      console.error(event);
+    };
+    this.socket.onclose = (_) => {
+      this.__setupSocketListeners();
+      this.events.dispatchEvent(new CustomEvent("disconnect"));
+    };
   }
 }
