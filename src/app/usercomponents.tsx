2970c09f92c2e4c83d17845e85c7f3cdc110efd2
Sergio <sergio@garciadelacruz.es>
2023-08-29 13:33:10 +0200

    Improve user links

--- src/app/usercomponents.tsx
+++ src/app/usercomponents.tsx
@@ -2,24 +2,32 @@ import { User } from "@/client/user";
 import { userColor } from "@/utils/namecolour";
 import { MouseEventHandler, useState } from "react";
 
-export function UsernameComponent({ user, alignRight, onClick }: { user: string, alignRight?: boolean, onClick?: MouseEventHandler<HTMLButtonElement> }){
+export function UsernameComponent(
+  { user, alignRight, onClick }: {
+    user: string;
+    alignRight?: boolean;
+    onClick?: MouseEventHandler<HTMLAnchorElement>;
+  },
+) {
   const [display, setDisplay] = useState<string>("hidden");
   const rank = user.charAt(0);
   const onclick = onClick ? onClick : () => {
-    setDisplay('')
+    setDisplay("");
   };
   return (
-    <button onClick={onclick}>
+    <>
       <span className="text-[#9D9488] whitespace-nowrap font-mono">
         &nbsp;
         {rank + (alignRight ? " " : "")}
       </span>
-      <span style={{ color: userColor(user) }} className="font-bold ">
-        {user.slice(1)}
-      </span>
-    </button>
+      <a onClick={onclick} style={{ cursor: "pointer" }}>
+        <span style={{ color: userColor(user) }} className="font-bold ">
+          {user.slice(1)}
+        </span>
+      </a>
+    </>
   );
-      // <UserCard user={user} />
+  // <UserCard user={user} />
 }
 
 export function UserCard({ user }: { user: User }) {
@@ -27,8 +35,8 @@ export function UserCard({ user }: { user: User }) {
     <div className="bg-gray-600 rounded-lg p-2">
       <UsernameComponent user={user.name} />
       <div className="text-xs text-gray-400">
-        {user.status} Hellooo helooooo 
+        {user.status} Hellooo helooooo
+      </div>
     </div>
-  </div>
-  )
+  );
 }
--- src/app/users.tsx
+++ src/app/users.tsx
@@ -23,6 +23,7 @@ export default function Users() {
   };
 
   const onClick = () => {
+    console.log("clicked");
   }
 
   useEffect(() => {
