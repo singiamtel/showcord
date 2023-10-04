8d70020d6b04bab03547b80d9553d88094963758
Sergio <sergio@garciadelacruz.es>
2023-10-05 00:26:47 +0200

    Config sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig(({ command }) => {
     const config = {
         base: `${process.env.PUBLIC_URL ?? ''}/`,
+        build: {
+            sourcemap: true, // Source map generation must be turned on
+        },
+        plugins: [
+            // Put the Sentry vite plugin after all other plugins
+            sentryVitePlugin({
+                authToken: process.env.SENTRY_AUTH_TOKEN,
+                org: 'na-q9f',
+                project: 'javascript-react',
+            }),
+        ],
     };
 
     if (command === 'build') {
@@ -16,7 +28,9 @@ export default defineConfig(({ command }) => {
     } else {
         return {
             ...config,
-            plugins: [react()],
+            // plugins: [react()],
+            plugins: [react(), ...config.plugins],
+            // swc options
         };
     }
 });
