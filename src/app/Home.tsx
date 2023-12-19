9ef9820a639a6bcd8e31f26550d507adefa69788
Sergio <sergio@garciadelacruz.es>
2023-12-19 04:17:51 +0100

    Start light/dark mode toggle

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,14 +2,19 @@ import Sidebar from './sidebar';
 import BigPanel from './BigPanel';
 import ToastProvider from './ToastProvider';
 import PS_contextProvider from './PS_context';
+import { useState } from 'react';
 
 export default function Home() {
+    const [darkMode, setDarkMode] = useState(true);
+    function toggleDarkMode() {
+        setDarkMode(!darkMode);
+    }
     return (
         <PS_contextProvider>
             <ToastProvider>
-                <div className="grid grid-cols-7 grid-rows-1 h-screen bg-gray-300 w-screen">
-                    <Sidebar className='col-span-1'/>
-                    <BigPanel className='col-span-6'/>
+                <div className={`grid grid-cols-7 grid-rows-1 h-screen bg-gray-300 w-screen ${darkMode ? '' : ''}`}>
+                    <Sidebar className="col-span-1" />
+                    <BigPanel className="col-span-6" />
                 </div>
             </ToastProvider>
         </PS_contextProvider>
