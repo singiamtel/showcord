8a534faacd361b76390c9a752067d680a2a27182
Sergio <sergio@garciadelacruz.es>
2023-12-21 02:29:17 +0100

    Fix weird scrol

--- src/UI/components/single/BigPanel.tsx
+++ src/UI/components/single/BigPanel.tsx
@@ -80,7 +80,7 @@ export default function BigPanel(props: HTMLAttributes<'div'>) {
             )}
         >
             <div className={'dark:bg-gray-300 flex flex-col w-full max-w-full'}>
-                <div className="h-[90%] max-h-[90%] flex-grow flex-shrink min-h-0 overflow-y-scroll">
+                <div className="h-[90%] max-h-[90%] flex-grow flex-shrink min-h-0">
                     <Chat
                         setUser={setUser}
                         username={username}
