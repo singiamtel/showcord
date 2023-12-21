b561d3f0484ba27bdfbcbd4ef759ae8493c4857b
Sergio <sergio@garciadelacruz.es>
2023-12-21 02:34:39 +0100

    Dark scrollbars!!

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -9,12 +9,14 @@ export default function Home() {
     function toggleDarkMode() {
         setDarkMode(!darkMode);
     }
+
+
     return (
         <div className={`${darkMode ? 'dark' : ''}`}>
             <PS_contextProvider>
                 <ToastProvider>
                     <div
-                        className={`grid grid-cols-7 grid-rows-1 h-screen text-black dark:text-white dark:bg-gray-300 w-screen `}
+                        className={`grid grid-cols-7 grid-rows-1 h-screen text-black dark:text-white dark:bg-gray-300 w-screen dark:[color-scheme:dark]`}
                     >
                         <Sidebar className="col-span-1" />
                         <BigPanel className="col-span-6" />
