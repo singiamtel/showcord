5586555f53616e311935a92f181fde4b0cfeec30
Sergio <sergio@garciadelacruz.es>
2024-01-23 18:18:05 +0100

    Add settings button while logged out

--- src/UI/components/single/userpanel.tsx
+++ src/UI/components/single/userpanel.tsx
@@ -15,49 +15,48 @@ export default function UserPanel() {
                 className={'rounded text-lg flex flex-row items-center h-auto w-full ' +
           (user ? ' w-auto p-2 ' : 'w-full font-bold ')}
             >
-                {user ?
-                    (
-                        <>
-                            <div className="flex flex-row justify-between items-center w-full">
+                <div className="flex flex-row justify-between items-center w-full">
+                    {user ?
 
-                                <div className="flex flex-row ">
-                                    <div className="w-12 h-12 rounded-full overflow-hidden mr-2 bg-gray-251 dark:bg-gray-250 border-2 border-gray-700">
-                                        <img
-                                            src={avatar ?
-                                                Sprites.getAvatar(avatar) :
-                                                Sprites.getAvatar(167)}
-                                            className="w-10 h-10 m-auto scale-[1.2]"
-                                        />
-                                    </div>
-                                    <div className="flex flex-col justify-center">
-                                        <span
-                                            style={{ color: userColor(user) }}
-                                            className="font-bold"
-                                        >
-                                            {user}
-                                        </span>
-                                    </div>
+                        (
+                            <div className="flex flex-row ">
+                                <div className="w-12 h-12 rounded-full overflow-hidden mr-2 bg-gray-251 dark:bg-gray-250 border-2 border-gray-700">
+                                    <img
+                                        src={avatar ?
+                                            Sprites.getAvatar(avatar) :
+                                            Sprites.getAvatar(167)}
+                                        className="w-10 h-10 m-auto scale-[1.2]"
+                                    />
                                 </div>
-
-                                <div className="flex flex-row justify-center items-center cursor-pointer hover-color" onClick={() => client.openSettings()}>
-                                    <FontAwesomeIcon icon={faCog} />
+                                <div className="flex flex-col justify-center">
+                                    <span
+                                        style={{ color: userColor(user) }}
+                                        className="font-bold"
+                                    >
+                                        {user}
+                                    </span>
                                 </div>
                             </div>
-                        </>
-                    ) :
-                    (
-                        <>
-                            <button
-                                onClick={() => {
-                                    client.login();
-                                    notificationsEngine.askPermission();
-                                }}
-                                className="font-bold rounded px-2 py-1 w-full"
-                            >
+
+                        ) :
+                        (
+                            <>
+                                <button
+                                    onClick={() => {
+                                        client.login();
+                                        notificationsEngine.askPermission();
+                                    }}
+                                    className="font-bold rounded px-2 py-1 w-full"
+                                >
                 Login
-                            </button>
-                        </>
-                    )}
+                                </button>
+                            </>
+                        )
+                    }
+                    <div className="flex flex-row justify-center items-center cursor-pointer hover-color" onClick={() => client.openSettings()}>
+                        <FontAwesomeIcon icon={faCog} />
+                    </div>
+                </div>
             </span>
         </div>
     );
