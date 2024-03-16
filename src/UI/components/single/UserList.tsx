2bffae52975461192179a7de5871ad05d1424fa6
Sergio <sergio@garciadelacruz.es>
2024-03-16 10:24:20 +0100

    Fix dark theme userlist

--- src/UI/components/single/UserList.tsx
+++ src/UI/components/single/UserList.tsx
@@ -48,7 +48,7 @@ export default function UserList({
     return (
         <div className="bg-gray-sidebar-light dark:bg-gray-600 w-full h-full p-2 overflow-y-auto whitespace-nowrap">
             <input className="w-full text-sm h-10 py-1 mb-2 px-2 bg-gray-251 placeholder:text-gray-150 dark:bg-gray-700 rounded-md" type="text" placeholder="Search users" value={search} onChange={(e) => setSearch(e.target.value)} />
-            {staff.length > 0 && <h2 className="text-sm font-bold text-gray-500 dark:text-gray-400 mb-2">Staff</h2>}
+            {staff.length > 0 && <h2 className="text-sm font-bold text-gray-500 dark:text-gray-175 mb-2">Staff</h2>}
             {staff.map((user, index) => (
                 <div key={index}>
                     <Username
@@ -63,7 +63,7 @@ export default function UserList({
             {
                 staff.length > 0 && regular.length > 0 && <hr className="my-2" />
             }
-            {regular.length > 0 && <h2 className="text-sm font-bold text-gray-500 dark:text-gray-400 mb-2">Users</h2>}
+            {regular.length > 0 && <h2 className="text-sm font-bold text-gray-500 dark:text-gray-175 mb-2">Users</h2>}
             {regular.map((user, index) => (
                 <div key={index}>
                     <Username
