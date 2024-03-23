95ba18e4efccbfe5d95cd3a09872c6119be1b6f4
Sergio <sergio@garciadelacruz.es>
2024-03-23 23:19:46 +0100

    Classname props

--- src/UI/components/Username.tsx
+++ src/UI/components/Username.tsx
@@ -1,14 +1,16 @@
+import { cn } from '@/lib/utils';
 import { userColor } from '../../utils/namecolour';
 import { useTrainerCard } from './single/TrainerCard/TrainerCardContext';
 
 export function Username(
-    { user, alignRight, colon, idle, bold, colorless }: Readonly<{
+    { user, alignRight, colon, idle, bold, colorless = false, className }: Readonly<{
         user: string;
         idle?: boolean;
         alignRight?: boolean;
         colon?: boolean;
         bold?: boolean;
         colorless?: boolean;
+        className?: string;
     }>,
 ) {
     const { clickUsername } = useTrainerCard();
@@ -29,8 +31,8 @@ export function Username(
                 style={{
                     color: colorless ? '' : idle ? '#888888' : userColor(user),
                 }}
-                className={'text-black dark:text-white hover:underline hover:cursor-pointer ' +
-                    (bold ? 'font-bold ' : '')}
+                className={cn('text-black dark:text-white hover:underline hover:cursor-pointer ',
+                    (bold ? 'font-bold ' : ''), className)}
                 data-message="true"
                 data-username={user.slice(1)}
             >
