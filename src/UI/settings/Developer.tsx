6c19c94c301dd1929ff096fa18cde2dbb3998eb2
Sergio <sergio@garciadelacruz.es>
2024-03-23 23:19:36 +0100

    Wrap settings page in a form

--- src/UI/settings/Developer.tsx
+++ src/UI/settings/Developer.tsx
@@ -12,40 +12,43 @@ export default function DeveloperSettings(props: Readonly<HTMLAttributes<'div'>>
     const [serverURL, setServerURL] = useState(client.settings.getServerURL());
     const [loginserverURL, setLoginserverURL] = useState(client.settings.getLoginServerURL());
     return (
-        <div className={twMerge('p-8', props.className)}>
-            <h2 className="text-xl pb-4">
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            client.settings.setServerURLs(serverURL || client.settings.defaultServerURL, loginserverURL || client.settings.defaultLoginServerURL);
+            setTimeout(() => {
+                window.location.reload();
+            }, 500);
+        }}>
+            <div className={twMerge('p-8', props.className)}>
+                <h2 className="text-xl pb-4">
               Developer settings
-            </h2>
-            <AlertDestructive title="Warning" description={description} />
-            <div className="pt-4">
-                <Label htmlFor="api-url">Server URL</Label>
-                <Input
-                    type={'url'}
-                    value={serverURL}
-                    placeholder={client.settings.defaultServerURL}
-                    onChange={(e) => setServerURL(e.target.value)}
+                </h2>
+                <AlertDestructive title="Warning" description={description} />
+                <div className="pt-4">
+                    <Label htmlFor="api-url">Server URL</Label>
+                    <Input
+                        type={'url'}
+                        value={serverURL}
+                        placeholder={client.settings.defaultServerURL}
+                        onChange={(e) => setServerURL(e.target.value)}
 
-                />
-            </div>
-            <div className="pt-4">
-                <Label htmlFor="api-url">Loginserver URL</Label>
-                <Input
-                    type={'url'}
-                    value={loginserverURL}
-                    placeholder={client.settings.defaultLoginServerURL}
-                    onChange={(e) => setLoginserverURL(e.target.value)}
-                />
-            </div>
-            <div className="pt-4" id="saveSettings">
-                <Button onClick={() => {
-                    client.settings.setServerURLs(serverURL || client.settings.defaultServerURL, loginserverURL || client.settings.defaultLoginServerURL);
-                    setTimeout(() => {
-                        window.location.reload();
-                    }, 500);
-                }}>
+                    />
+                </div>
+                <div className="pt-4">
+                    <Label htmlFor="api-url">Loginserver URL</Label>
+                    <Input
+                        type={'url'}
+                        value={loginserverURL}
+                        placeholder={client.settings.defaultLoginServerURL}
+                        onChange={(e) => setLoginserverURL(e.target.value)}
+                    />
+                </div>
+                <div className="pt-4" id="saveSettings">
+                    <Button type="submit">
                   Save changes and refresh
-                </Button>
+                    </Button>
+                </div>
             </div>
-        </div>
+        </form>
     );
 }
