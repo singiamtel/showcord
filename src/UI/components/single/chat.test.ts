af211ff54c5ae15e457394c2311dc3243c8d76c4
Sergio <sergio@garciadelacruz.es>
2024-03-24 09:28:54 +0100

    Add some more tests

--- src/UI/components/single/chat.test.ts
+++ src/UI/components/single/chat.test.ts
@@ -9,5 +9,30 @@ suite('inlineCode', () => {
         // console.log(container.querySelector('code'));
         expect(container.querySelector('code')?.textContent).toEqual('test');
     });
+
+    test('should render many different code elements', () => {
+        const message = 'This can be ```used``` to see if the ```bug``` if ```still``` present or it is ```solved```';
+        const expected = ['used', 'bug', 'still', 'solved'];
+        const JSX = FormatMsgDisplay({ msg: message });
+        const { container } = render(JSX);
+        // console.log(container.querySelector('code'));
+        const codeElements = container.querySelectorAll('code');
+        expect(codeElements.length).toEqual(expected.length);
+        codeElements.forEach((el, i) => {
+            expect(el.textContent).toEqual(expected[i]);
+        });
+    });
+
+    test('should work with multiple backticks', () => {
+        const message = '`one` ``two`` ```three``` ````four````';
+        const expected = ['one', 'two', 'three', 'four'];
+        const JSX = FormatMsgDisplay({ msg: message });
+        const { container } = render(JSX);
+        const codeElements = container.querySelectorAll('code');
+        expect(codeElements.length).toEqual(expected.length);
+        codeElements.forEach((el, i) => {
+            expect(el.textContent).toEqual(expected[i]);
+        });
+    });
 });
 
