bbc357a2e63500c9947dbb9f986a5a75118e7fe6
Sergio <sergio@garciadelacruz.es>
2024-04-08 01:33:45 +0200

    Fix lint

--- src/UI/components/ErrorHandler.tsx
+++ src/UI/components/ErrorHandler.tsx
@@ -1,4 +1,4 @@
-export function ErrorHandler({error}: {error: Error}) {
+export function ErrorHandler({ error }: {error: Error}) {
     return <div className='w-full h-full flex flex-col justify-center items-center'>
         <h1 className='text-2xl font-bold'>Something went wrong</h1>
         <p className='text-lg'>{error.message}</p>
