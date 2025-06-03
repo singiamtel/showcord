2b57a0baef4768b65428b9726b4752ab07cc3c17
Sergio Garcia <sergio@garciadelacruz.es>
2025-06-03 17:01:36 +0200

    Show date on news

--- src/UI/components/NewsCard.tsx
+++ src/UI/components/NewsCard.tsx
@@ -14,12 +14,18 @@ export default function NewsCard(
 ) {
     return (
         <div className="m-2">
-            <h2 className="font-bold text-lg">
+            <h2 className="font-bold text-lg pb-0">
                 {news.title}
             </h2>
             <span className="ml-2 mb-2 block">
                 <span className="text-gray-125">
-                    {news.author}
+                    {/* The date comes in as a unix timestamp, so we need to convert it to a date */}
+                    {/* FIXME: This gives different dates than the official client. (can't tell if it's us doing it wrong) */}
+                    {news.author} on {new Date(news.date * 1000).toLocaleDateString('en-US', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit',
+                    })}
                 </span>
                 <HTML message={news.summaryHTML} raw />
             </span>
