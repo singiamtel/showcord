44938e7b3fd4eada9234bf354cc220d4a08d5ca9
Sergio Garcia <sergio@garciadelacruz.es>
2026-05-21 18:40:29 +0200

    Only import Roboto Mono latin-400 instead of all weights

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import App from './UI/App';
 import { StrictMode } from 'react';
 import { client } from './client/singleton';
 
-import '@fontsource/roboto-mono/index.css';
+import '@fontsource/roboto-mono/latin-400.css';
 import './UI/globals.css';
 import ClientContextProvider from './UI/components/single/ClientContext';
 import { Analytics } from '@vercel/analytics/react';
